9ad4bf0313569a8f4822897619a8c73663afc346
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-07-09 08:44:40 +0300

    fix: ex7.js solved

---
+++ ex7.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function isPalindrome(s) {
+    if (!s || s.length === 1) return true;
+
+    if (s.at(0) !== s.at(-1)) return false
+
+    return isPalindrome(s.slice(1, -1));
+}
+
+console.log( isPalindrome("") === true );
+console.log( isPalindrome("a") === true );
+console.log( isPalindrome("aa") === true );
+console.log( isPalindrome("aba") === true );
+console.log( isPalindrome("abba") === true );
+console.log( isPalindrome("abccba") === true );
+
+console.log( isPalindrome("ab") === false );
+console.log( isPalindrome("abc") === false );
+console.log( isPalindrome("abca") === false );
+console.log( isPalindrome("abcdba") === false );
